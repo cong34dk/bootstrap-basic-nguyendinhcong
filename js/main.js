4dffa5910a6383318bc2a1ec53d2ecdbb00c13ba
cong34dk <nguyencong12g2003@gmail.com>
2024-07-22 17:06:03 +0700

    update code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// Set cho header có chiều cao bằng 100% chiều cao màn hình
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
+
+//click vào thẻ <a> trong phần navbar thì trang sẽ cuộn mượt mà đến vị trí tương ứng:
+$(document).ready(function(){
+    $('nav a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
